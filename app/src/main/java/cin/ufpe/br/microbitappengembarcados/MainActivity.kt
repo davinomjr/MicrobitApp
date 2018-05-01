5d422b45e70322830015e6f526fe7e55210523ea
Davino Junior <dmtsj@cin.ufpe.br>
2018-05-01 00:19:15 -0300

    creating project

---
+++ app/src/main/java/cin/ufpe/br/microbitappengembarcados/MainActivity.kt
@@ -0,0 +1,16 @@
+package cin.ufpe.br.microbitappengembarcados
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun helloWorld(message: String){
+
+    }
+}
